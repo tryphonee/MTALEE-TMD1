8170b6ea09005a1862063a80f9d09ad00e84ff4d
MATELEE <tryphonee3@gmail.com>
2025-07-02 10:23:01 +0300

    Update set.js

--- set.js
+++ set.js
@@ -9,7 +9,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "𝚳𝚫𝚻𝚵𝐋𝚵𝚵-𝚻𝚳𝐃",
+    OWNER_NAME: process.env.OWNER_NAME || "𝚳𝚫𝚻𝚵𝐋𝚵𝚵",
     NUMERO_OWNER : process.env.NUMERO_OWNER || "MATELEE TMD",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
