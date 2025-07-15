9e60f3bd887359489ffd987b1ec4b18a21c4c0ac
charlesxmd <xmdmojayamwisho@gmail.com>
2025-07-16 01:25:15 +0300

    Update zokou.js

--- framework/zokou.js
+++ framework/zokou.js
@@ -6,7 +6,7 @@ function zokou(obj, fonctions) {
         infoComs.categorie = "General";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "🗂️";
+        infoComs.reaction = "🌟";
     }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
