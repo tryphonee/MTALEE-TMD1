fd8020023e26302e15ddf08a0b1814fba935dc9c
Next5x/timnasa <botuse41@gmail.com>
2025-12-05 20:53:04 +0300

    Update speed.js

--- commandes/speed.js
+++ commandes/speed.js
@@ -15,7 +15,7 @@ zokou({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('*TIMNASA-MD-SPEED-IS*\n ```' +99999999999+ '``` *ᴍ/s*') 
+    return repondre('*ₘₐₜₑₗₑₑ ₚᵢₙg ₚₒₙg ₛₚₑₑd .₉₈%*\n ```' +98%+ '``` *ᴍ/s*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
