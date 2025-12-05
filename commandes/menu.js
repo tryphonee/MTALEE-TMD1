a06737bc37832ccec16e061b97b907890fec950e
Next5x/timnasa <botuse41@gmail.com>
2025-12-05 16:35:50 +0300

    Update menu.js

--- commandes/menu.js
+++ commandes/menu.js
@@ -7,68 +7,80 @@ const moment = require("moment-timezone");
 const s = require(__dirname + "/../set");
 const more = String.fromCharCode(8206)
 const readmore = more.repeat(4001)
-zokou({ nomCom: "menu", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
+
+zokou({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions) => {
     let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
     let { cm } = require(__dirname + "/../framework//zokou");
     var coms = {};
     var mode = "public";
+    
     if ((s.MODE).toLocaleLowerCase() != "yes") {
         mode = "private";
     }
+
+
+    
+
     cm.map(async (com, index) => {
         if (!coms[com.categorie])
             coms[com.categorie] = [];
         coms[com.categorie].push(com.nomCom);
     });
+
     moment.tz.setDefault('Etc/GMT');
+
 // Créer une date et une heure en GMT
 const temps = moment().format('HH:mm:ss');
 const date = moment().format('DD/MM/YYYY');
+
   let infoMsg =  `
-╭━═「 *${s.BOT}* 」═━❂
-┃⊛╭───────────➻
-┃⊛│◆ 𝙾𝚠𝚗𝚎𝚛 : ${s.OWNER_NAME}
-┃⊛│◆ 𝙿𝚛𝚎𝚏𝚒𝚡 : [ ${s.PREFIXE} ] 
-┃⊛│◆ 𝙼𝚘𝚍𝚎 : *${mode}*
-┃⊛│◆ 𝚁𝚊𝚖  : 𝟴/𝟭𝟯𝟮 𝗚𝗕
-┃⊛│◆ 𝙳𝚊𝚝𝚎  : *${date}* 
-┃⊛│◆ 𝙿𝚕𝚊𝚝𝚏𝚘𝚛𝚖 : ${os.platform()}
-┃⊛│◆ 𝙲𝚛𝚎𝚊𝚝𝚘𝚛 : ᴍᴀᴛᴇʟᴇᴇ
-┃⊛│◆ 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜 : ${cm.length}
-┃⊛│◆ 𝚃𝚑𝚎𝚖𝚎 : matelee
-┃⊛└───────────➻
-╰─━━━━══─⊷─══━━━❂\n${readmore}`;
-    let menuMsg = `ᴍᴀᴛᴇʟᴇᴇ ᴛᴍᴅ ᴄᴏᴍᴍᴀɴᴅs`;
-    for (const cat in coms) {
-        menuMsg += `
-╭━━═✺ *${cat}* ✺═━━⊷ 
-┊│┌═━━⊷•∞•⊷━━─⊛
-┊│┊
-┌┤┊ `;for (const cmd of coms[cat]) {
-          menuMsg += `          
-┊│┊☆  *${cmd}*`    
-        } 
-        menuMsg +=`
-┊│└═━━⊷•∞•⊷━━─⊛  
-╰━━━═⊷✺•∞•✺⊷═━━━⊷`
-    }
-    menuMsg += `
-> ᴘᴏᴡᴇʀ ʙʏ ᴍᴀᴛᴇʟᴇᴇ ᴛᴍᴅ\n
+>  HANS-MD AVAILABLE MENUS* 
+╭─────────────────
+│❒⁠⁠⁠⁠╭─────────────
+│❒⁠⁠⁠⁠│▸ *MENU* 
+│❒⁠⁠⁠⁠│▸ *MENU2* 
+│❒⁠⁠⁠⁠│▸ *BUGMENU*
+│❒⁠⁠⁠⁠╰──────────────
+│❒⁠⁠⁠⁠│▸ *PLUGINS* : ${cm.length} 
+│❒⁠⁠⁠⁠│▸ *RAM* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
+│❒⁠⁠⁠⁠│▸ *SAVER* : ${os.platform()}
+│❒⁠⁠⁠⁠│▸ *THEME* : *ANDBADTHEMES*
+│❒⁠⁠⁠⁠╰──────────────
+╰──────────────────\n`;
+    
+let menuMsg = `
+
+ *COMMANDS*${readmore}
 `;
+
+    for (const cat in coms) {
+        menuMsg += ` ╭────────❒⁠⁠⁠⁠ *${cat}* ✣`;
+        for (const cmd of coms[cat]) {
+            menuMsg += `
+│❒⁠⁠⁠⁠│▸ ${cmd}`;
+        }
+        menuMsg += `
+╰────────────···▸▸ \n`
+    }
+
+    menuMsg += `> MADE BY HANS TECH
+`;
+
    var lien = mybotpic();
+
    if (lien.match(/\.(mp4|gif)$/i)) {
     try {
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Luckymd*, déveloper Fredie Tech" , gifPlayback : true }, { quoted: ms });
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *hans-md*, déveloper hans Tech" , gifPlayback : true }, { quoted: ms });
     }
     catch (e) {
-       console.log("🥵🥵 Menu erreur " + e);
+        console.log("🥵🥵 Menu erreur " + e);
         repondre("🥵🥵 Menu erreur " + e);
     }
 } 
 // Vérification pour .jpeg ou .png
 else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
     try {
-        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Luckymd*, déveloper Fredie Tech" }, { quoted: ms });
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *hanstz*, déveloper hans Tech" }, { quoted: ms });
     }
     catch (e) {
         console.log("🥵🥵 Menu erreur " + e);
@@ -76,6 +88,9 @@ else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
     }
 } 
 else {
+    
     repondre(infoMsg + menuMsg);
+    
 }
+
 });
