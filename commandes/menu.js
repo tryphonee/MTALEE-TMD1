6a74abff873006aebd55eea7973d8c41fc5a0fcd
Qartde <bonifacemd2525@gmail.com>
2025-12-05 21:25:58 +0300

    Update menu.js

--- commandes/menu.js
+++ commandes/menu.js
@@ -5,86 +5,70 @@ const { format } = require(__dirname + "/../framework/mesfonctions");
 const os = require("os");
 const moment = require("moment-timezone");
 const s = require(__dirname + "/../set");
-
-zokou({ nomCom: "menu2", categorie: "General" }, async (dest, zk, commandeOptions) => {
+const more = String.fromCharCode(8206)
+const readmore = more.repeat(4001)
+zokou({ nomCom: "menu", categorie: "Menu" }, async (dest, zk, commandeOptions) => {
     let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
     let { cm } = require(__dirname + "/../framework//zokou");
     var coms = {};
     var mode = "public";
-    
     if ((s.MODE).toLocaleLowerCase() != "yes") {
         mode = "private";
     }
-
-
-    
-
     cm.map(async (com, index) => {
         if (!coms[com.categorie])
             coms[com.categorie] = [];
         coms[com.categorie].push(com.nomCom);
     });
-
     moment.tz.setDefault('Etc/GMT');
-
 // Créer une date et une heure en GMT
 const temps = moment().format('HH:mm:ss');
 const date = moment().format('DD/MM/YYYY');
-
   let infoMsg =  `
-╭────《《HANS-MD》》────
-┴  ╭─────────────
-│❒⁠⁠⁠⁠│ *ADMIN* : ${s.OWNER_NAME}
-│❒│⁠⁠⁠⁠ *CALENDER* : ${date}
-│❒│⁠⁠⁠⁠ *PREFIX* : ${s.PREFIXE}
-│❒⁠⁠⁠⁠│⁠⁠⁠ *BOT IS IN* : ${mode} mode
-│❒│⁠⁠⁠⁠ *ORDERS* : ${cm.length} 
-│❒│⁠⁠⁠⁠ *SPACE* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
-│❒│⁠⁠⁠⁠ *CHROME* : ${os.platform()}
-│❒│⁠⁠⁠⁠ *THEME* : *HANSTZ*
-┬  ╰──────────────
-╰─── ··《《HANS-MD》》··──\n`;
-    
-let menuMsg = `
- ─────────
-  *BOT BY HANSTZ TECH* 
- ─────────
-
-
- *ℂ𝕆𝕄𝕄𝔸ℕ𝔻𝕊*
-`;
-
+╭━═「 *${s.BOT}* 」═━❂
+┃⊛╭───────────➻
+┃⊛│◆ 𝙾𝚠𝚗𝚎𝚛 : ${s.OWNER_NAME}
+┃⊛│◆ 𝙿𝚛𝚎𝚏𝚒𝚡 : [ ${s.PREFIXE} ] 
+┃⊛│◆ 𝙼𝚘𝚍𝚎 : *${mode}*
+┃⊛│◆ 𝚁𝚊𝚖  : 𝟴/𝟭𝟯𝟮 𝗚𝗕
+┃⊛│◆ 𝙳𝚊𝚝𝚎  : *${date}* 
+┃⊛│◆ 𝙿𝚕𝚊𝚝𝚏𝚘𝚛𝚖 : ${os.platform()}
+┃⊛│◆ 𝙲𝚛𝚎𝚊𝚝𝚘𝚛 : ᴍᴀᴛᴇʟᴇᴇ
+┃⊛│◆ 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜 : ${cm.length}
+┃⊛│◆ 𝚃𝚑𝚎𝚖𝚎 : 𝗠𝗔𝗧𝗘𝗟𝗘𝗘
+┃⊛└───────────➻
+╰─━━━━══─⊷─══━━━❂\n${readmore}`;
+    let menuMsg = `𝗠𝗔𝗧𝗘𝗟𝗘𝗘 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 𝗯𝗼𝘁 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀`;
     for (const cat in coms) {
-        menuMsg += ` ╭─⬡ *${cat}* ⬡─`;
-        for (const cmd of coms[cat]) {
-            menuMsg += `
-⬡│▸ *${cmd}*`;
-        }
         menuMsg += `
-  ╰────────────·· \n`
+╭━━═✺ *${cat}* ✺═━━⊷ 
+┊│┌═━━⊷•∞•⊷━━─⊛
+┊│┊
+┌┤┊ `;for (const cmd of coms[cat]) {
+          menuMsg += `          
+┊│┊☆  *${cmd}*`    
+        } 
+        menuMsg +=`
+┊│└═━━⊷•∞•⊷━━─⊛  
+╰━━━═⊷✺•∞•✺⊷═━━━⊷`
     }
-
     menuMsg += `
-
-|⏣MADE BY HANS TECH
-*❒⁠⁠⁠⁠—————————— ❒⁠⁠⁠⁠——————————❒⁠⁠⁠⁠*
+> 𝗠𝗔𝗧𝗘𝗟𝗘𝗘 𝗺𝗮𝘀𝘁𝗲𝗿 𝗼𝗳 𝗱𝗶𝗴𝗶𝘁𝗮𝗹 𝗵𝗮𝗰𝗸𝘀\n
 `;
-
    var lien = mybotpic();
-
    if (lien.match(/\.(mp4|gif)$/i)) {
     try {
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *hans-md*, déveloper hans Tech" , gifPlayback : true }, { quoted: ms });
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Luckymd*, déveloper Fredie Tech" , gifPlayback : true }, { quoted: ms });
     }
     catch (e) {
-        console.log("🥵🥵 Menu erreur " + e);
+       console.log("🥵🥵 Menu erreur " + e);
         repondre("🥵🥵 Menu erreur " + e);
     }
 } 
 // Vérification pour .jpeg ou .png
 else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
     try {
-        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *hans-md*, déveloper Hans Tech" }, { quoted: ms });
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Luckymd*, déveloper Fredie Tech" }, { quoted: ms });
     }
     catch (e) {
         console.log("🥵🥵 Menu erreur " + e);
@@ -92,9 +76,6 @@ else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
     }
 } 
 else {
-    
     repondre(infoMsg + menuMsg);
-    
 }
-
 });
