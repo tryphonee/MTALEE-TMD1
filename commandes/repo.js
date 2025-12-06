d47e6b8028d3f4641a4c3441bbdc03a2d9842d6a
Qartde <bonifacemd2525@gmail.com>
2025-12-06 09:20:13 +0300

    Update repo.js

--- commandes/repo.js
+++ commandes/repo.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
-zokou({ nomCom: "repo", catégorie:"Général", reaction: "📑", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-  const githubRepo = 'https://api.github.com/repos/betingrich/Tesla';
-  const img = 'https://telegra.ph/file/db890d1a98e3ab7ef631a.jpg';
+zokou({ nomCom: "repo", catégorie:"Général", reaction: "❄", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+  const githubRepo = 'https://github.com/tryphonee/MTALEE-TMD1';
+  const img = 'https://files.catbox.moe/ejm45q.jpg';
 
   try {
     const response = await fetch(githubRepo);
@@ -21,16 +21,17 @@ zokou({ nomCom: "repo", catégorie:"Général", reaction: "📑", nomFichier: __
       const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
       const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
 
-      const gitdata = `*𝗛𝗶, 𝗜 𝗮𝗺* *𝗧𝗲𝘀𝗹𝗮.*\n  
-🏎️ *𝗣𝗮𝗶𝗿 𝗰𝗼𝗱* https://bugatti-session.onrender.com
-🏎️ *𝗥𝗲𝗽𝗼:* ${data.html_url}
-🏎️ *𝗦𝘁𝗮𝗿𝘀:* ${repoInfo.stars}
-🏎️ *𝗙𝗼𝗿𝗸𝘀:* ${repoInfo.forks}
-🏎️ *𝗥𝗲𝗹𝗲𝗮𝘀𝗲 𝗗𝗮𝘁𝗲:* ${releaseDate}
-🏎️ *𝗨𝗽𝗱𝗮𝘁𝗲𝗱 𝗼𝗻:* ${repoInfo.lastUpdate}
-🏎️ *𝗢𝘄𝗻𝗲𝗿:* *𝗠𝗮𝗿𝗶𝘀𝗲𝗹*
+      const gitdata = `*𝗛𝗶, 𝗜 𝗮𝗺* *𝗠𝗮𝘁𝗲𝗹𝗲𝗲.*\n  
+🎲 *𝗣𝗮𝗶𝗿 𝗰𝗼𝗱* https://timnasa-session-id-etyy.onrender.com/
+♦ *𝗥𝗲𝗽𝗼:* ${data.html_url}
+🌟 *𝗦𝘁𝗮𝗿𝘀:* ${repoInfo.stars}
+🪡 *𝗙𝗼𝗿𝗸𝘀:* ${repoInfo.forks}
+🎯 *𝗥𝗲𝗹𝗲𝗮𝘀𝗲 𝗗𝗮𝘁𝗲:* ${releaseDate}
+✅ *𝗨𝗽𝗱𝗮𝘁𝗲𝗱 𝗼𝗻:* ${repoInfo.lastUpdate}
+💫 *𝗢𝘄𝗻𝗲𝗿:* *𝗠𝗮𝘁𝗲𝗹𝗲𝗲*
+📡 *𝗪𝗰𝗵𝗮𝗻𝗻𝗲𝗹* https://whatsapp.com/channel/0029VatokI45EjxufALmY32X
 __________________________________
-            *𝗧𝗘𝗦𝗟𝗔*`;
+            *𝚳𝚫𝚻𝚵𝐋𝚵𝚵-𝚻𝚳𝐃*`;
 
       await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
     } else {
